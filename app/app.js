03a20e011efd5b7d26280665337c31fec252823f
alex <desorbaix@free.fr>
2020-06-25 15:14:37 +0200

    up des nom de ressources et forme des ressources name

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
-const REST_SERVER='https://cors-anywhere.herokuapp.com/https://api-ratp.pierre-grimaud.fr/v4';
-
+// const REST_SERVER='https://cors-anywhere.herokuapp.com/https://api-ratp.pierre-grimaud.fr/v4';
+const REST_SERVER='http://localhost:5000';
+const MA_STATION='la+defense+(grande+arche)';
 window.lines={metros:[],tramways:[],rers:[],buses:[],noctiliens:[]};
-window.transportType=['metros','tramways','rers','buses','noctiliens'];
+window.transportType=['metros'];//,'tramways','rers','buses','noctiliens'];
 
 //recuperation de tout le parc de lignes
 function getAllLines(params) {
@@ -9,7 +10,11 @@ function getAllLines(params) {
     transportType.forEach(type => {
         (new RestCrud(REST_SERVER)).get(lines=>{
             lines.forEach(line=>{
-                getStations(e=>console.log(e),type,line.code)
+                getStations(stations=>{
+                   var arrayStationFound= stations.find(station=>{if(station.slug==MA_STATION)return true;})
+                    
+                   if(arrayStationFound.length)window.lines.metros.push(line);
+                },type,line.code)
             })
         },'/lines/'+type);    
     });
@@ -17,6 +22,8 @@ function getAllLines(params) {
 
 function getSchedules(type,line,station, ar='R')
 {
-    (new RestCrud(REST_SERVER)).get(e=>{console.log(e)},'/schedules/'+type,line+'/'+station+'/'+ar);   
+    var ressourceUrl='/schedules_'+type+'_'+line+'_'+station+'_'+ar;
+    // console.log(REST_SERVER+ressourceUrl)
+    (new RestCrud(REST_SERVER)).get(e=>{},ressourceUrl);   
 }
-getSchedules('metros','1','la+defense+(grande+arche)','A')
+getSchedules('rers','a','defense','R')
